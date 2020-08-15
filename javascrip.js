3831f408d3778b16ff360b33a25b0f81837f7175
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-08-14 23:27:57 -0500

    updated the first question

--- javascrip.js
+++ javascrip.js
@@ -26,20 +26,44 @@ var questionKey = Object.keys(questions);
 
 var startEl = document.getElementById("start-btn");
 var questionEl = document.getElementById("questions");
+var questionConEl = document.getElementById("questions-contanier");
+
+//this will track which question the game is on
+var questionTracker = 0;
 
 startEl.addEventListener('click', startGame);
   
-  
+  //this function will start the game
 function startGame(){
   console.log("inside the start button");
   startEl.classList.add("hide");
   startQuestions(); 
-  
+  printAnswers();
 
 };
 
+//this function will print the questions to the screen
+
 function startQuestions(){
-   questionEl.textContent.questionArray;
+   questionConEl.classList.remove("hide");
+   questionEl.innerHTML = questions[questionTracker].question;
+   append
    console.log("inside questions");
 
 };
+
+function printAnswers(){
+console.log("starting the array ");
+for(var i =0;i < questionArray.length; i++){
+  console.log(questionKey[i]);
+  console.log("in the loop");
+}
+ 
+
+ /* for (var i = 0; i < .length; i++) {
+    var btn = document.createElement("answer-buttons");
+    var t = document.textcontent(listBrand[i]);
+    btn.appendChild(t);
+    document.body.appendChild(btn);
+*/
+}
