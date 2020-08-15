1847ea8d69f3419fea3988e329b77772a97d6b9e
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-08-14 21:27:17 -0500

    changed html and js

--- javascrip.js
+++ javascrip.js
@@ -1,6 +1,6 @@
 var questions = [
   {
-      question: 'artist of the year',
+      question: 'Who was the artist of the year in 2019? ',
       answerChoices: ['Billie Ellish', 'Ariana Grande', 'Drake', 'Elton John'],
       correctAnswer: 'Billie Ellish',
   },
@@ -11,56 +11,37 @@ var questions = [
      correctAnswer: 'Whale Shark',
 
   },
+  {
+    question: "How many gills does a Great White Shark have? ",
+    answerChoices:["8", "4", "7", "5"],
+    correctAnswer: "5",
 
+  },
+    
 ]
 //this array will keep th
-var questionArray = [ questions, questions2, questions3];
+//var questionArray = [ questions, questions2, questions3];
+var questionArray = Object.values(questions);
+var questionKey = Object.keys(questions);
 
-var win = 0;
-var losses = 0;
- 
-function buttonStart(){
+var startEl = document.getElementsByName('start-btn');
 
-  //start the timer
+startEl.addEventListener('click', startGame);
+  
+    
+
+
+
+function startGame(){
+  console.log("inside the start button");
+   
+  
 
 };
+startGame();
 
-
-var mainEl = document.getElementById("main");
-var questionEl = document.createElement("div");
-var increment = 0;
-
-questionEl.textContent = questions[increment].question;
-mainEl.append(questionEl);
-
-
-for(var i =0; i < questions[increment].answerChoices.length; i++){
- // questionEl.textContent = questions[increment].question;
-  //mainEl.append(questionEl);
-  
-  var answerChoiceEl =document.createElement("div");
-  answerChoiceEl.addEventListener('click', function(){
-     var answerSelected = this.textContent;
-     if(answerSelected === questions[increment].correctAnswer){
-        console.log("Congradulations you are Correct!!!!");
-       }else{
-         console.log("Sorry you are incorrect");
-     }
-    
-     
-
-  });
-
-  answerChoiceEl.textContent = questions[increment].answerChoices[i];
-  mainEl.append(answerChoiceEl);
-
-}
-
-
-
-function endGame(){
-  clearInterval(countdown);
-   // display score
+function startQuestions(){
+   
 
 
 }
